26e196703b64a597d23d19a1a345a5697f333bd9
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-11-29 23:06:55 +0500

    homework

---
+++ index.js
@@ -0,0 +1,313 @@
+// 1 - misol
+
+// let a = 32;
+// let b = 23;
+
+// let c = Math.trunc(a/10)
+// let d = Math.trunc(b/10)
+
+// let res;
+
+// if (c > d) {
+//     res = c
+// }else{
+//     res =  d
+// }
+
+// console.log(res);
+
+// 2 - misol
+
+// let a = 324;
+
+// let b = a % 10;
+// let c = Math.trunc(a/10)%10;
+// let d = Math.trunc(a/100);
+
+// let res;
+
+// if (b < c) {
+//     res = b
+// }
+// if (d < c) {
+//     res = d
+// }else{
+//     res = c
+// }
+
+// console.log(res);
+
+
+// 3 - misol
+
+// let a = 364;
+
+// let c = Math.trunc(a/10)%10;
+
+// let res;
+
+// if (c % 3 == 0) {
+//     res = "Rost"
+// }else{
+//     res = "Yolg'on"
+// }
+
+// console.log(res);
+
+
+// 4 - misol
+
+// let a = 4;
+// let b = 3;
+// let c = 5;
+
+// let res;
+
+// if (a < b) {
+//     res = a;
+// };
+// if (b < c) {
+//     res = b;
+// }else{
+//     res = c;
+// }
+
+// console.log(res ** 3);
+
+
+// 5 - misol
+
+// let a = 97;
+// let b = 8;
+// let d = 1;
+// let c = 54;
+
+// let res;
+
+// if (a > b) {
+//     res = a;
+// }else{
+//     res = b
+// }
+// if (res < c){
+//     res = c;
+// }
+// if (res < d){
+//     res = d;
+// }
+
+// console.log(res);
+
+
+// 6 - misol
+
+// let a = 95;
+// let b = 56;
+// let c = 43;
+// let sum = a + b + c;
+// let res;
+// let min;
+// let max;
+
+// if (a > b) {
+//     max = a
+// }
+// else {
+//     max = b
+// }
+// if (c > max){
+//     max = c
+// }
+
+// if (a < b) {
+//     min = a
+// }
+// else {
+//     min = b
+// }
+// if (c < min){
+//     min = c
+// }
+
+//  res = sum - (min+max)
+
+// console.log(res);
+
+
+// 7 - misol
+
+// let a = 195;
+// let b = 526;
+// let c = 143;
+// let sum = a + b + c;
+// let res;
+// let min;
+// let max;
+
+// if (a > b) {
+//     max = a
+// }
+// else {
+//     max = b
+// }
+// if (c > max){
+//     max = c
+// }
+
+// if (a < b) {
+//     min = a
+// }
+// else {
+//     min = b
+// }
+// if (c < min){
+//     min = c
+// }
+
+//  res = sum - (min+max)
+
+// console.log(max);
+// console.log(res);
+
+
+// 8 - misol
+
+// let a = 9;
+// let b = 9;
+// let c = 12;
+// let d = 30;
+// let e = 15;
+
+// let res = 0;
+
+// if (a % 3 == 0) {
+//     res = res + 1
+// }
+// if (b % 3 == 0) {
+//     res = res + 1
+// }
+// if (c % 3 == 0) {
+//     res = res + 1
+// }
+// if (d % 3 == 0) {
+//     res = res + 1
+// }
+// if (e % 3 == 0) {
+//     res = res + 1
+// }
+
+// console.log(res);
+
+
+// 9 - misol
+
+// let a = 9;
+// let b = 9;
+// let c = 2;
+// let d = 30;
+
+// let res = 0;
+
+// if (a % 3 == 0) {
+//     res = res + 1
+// }
+// if (b % 3 == 0) {
+//     res = res + 1
+// }
+// if (c % 3 == 0) {
+//     res = res + 1
+// }
+// if (d % 3 == 0) {
+//     res = res + 1
+// }
+
+// if (a % 5 == 0) {
+//     res = res + 1
+
+//     if (a % 3 == 0) {
+//         res = res - 1
+//     }
+// }
+// if (b % 5 == 0) {
+//     res = res + 1
+
+//     if (b % 3 == 0) {
+//         res = res - 1
+//     }
+// }
+// if (c % 5 == 0) {
+//     res = res + 1
+
+//     if (c % 3 == 0) {
+//         res = res - 1
+//     }
+// }
+// if (d % 5 == 0) {
+//     res = res + 1
+
+//     if (d % 3 == 0) {
+//         res = res - 1
+//     }
+// }
+
+// console.log(res);
+
+
+// 10 - misol
+
+// let a = 9;
+// let b = 9;
+// let c = 2;
+
+// let res;
+
+// if (a != 0) {
+//     res =  a
+// }
+
+// if (b != 0) {
+//     res =  b
+// }
+
+// if (b != 0) {
+//     res =  b
+// }
+
+// res = a + b + c;
+
+// console.log(res);
+
+
+// 11 - misol
+
+// let a = 634;
+
+// let b = a % 10
+// let o = Math.floor(a/10)%10
+// let y = Math.floor(a/100)
+
+// let res = 1 ;
+
+// if (b % 2 == 0) {
+//     res *= b
+// }
+// if (o % 2 == 0) {
+//     res *= o
+// }
+// if (y % 2 == 0) {
+//     res *=  y
+// }
+
+// console.log(res);
+
+
+// 12 - misol
+
+// let a = 9;
+// let b = 9;
+// let c = 2;
+
+// sum = a + b + c;
+// res = sum / 3;
+
+// console.log(res);
